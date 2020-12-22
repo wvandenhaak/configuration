feb939741b1f3eca9cd72c747d5491ab26372964
Wesley van den Haak <w.vandenhaak@live.nl>
2020-12-22 01:54:47 +0100

    Added basic test for Setup Object

--- tests/Setup/Model/SetupTest.php
+++ tests/Setup/Model/SetupTest.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace IceCake\AppConfigurator\Tests\Setup\Model;
 
+use IceCake\AppConfigurator\Setup\Model\Group\GroupCollection;
+use IceCake\AppConfigurator\Setup\Model\Option\OptionCollection;
 use IceCake\AppConfigurator\Setup\Model\Setup;
 use PHPUnit\Framework\TestCase;
 
@@ -14,5 +16,19 @@ use PHPUnit\Framework\TestCase;
  */
 class SetupTest extends TestCase
 {
-    
+
+    /**
+     * Test if a Setup object can be created and returns the same values
+     * @return void
+     */
+    public function testCanCreate(): void
+    {
+        $options = $this->createMock(OptionCollection::class);
+        $groups = $this->createMock(GroupCollection::class);
+
+        $subject = new Setup($options, $groups);
+
+        $this->assertSame($options, $subject->getOptions());
+        $this->assertSame($groups, $subject->getGroups());
+    }
 }
